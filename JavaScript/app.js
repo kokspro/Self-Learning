3d0ed74b53bc5d2f87e04bc89a0b10bbdfcc96a1
Kokspro <98177061+kokspro@users.noreply.github.com>
2025-07-29 17:42:45 -0500

    Operators Section Finished

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,23 @@
 // console.log("Ayyyy, links working");
 
 //Ternary Operators
-let points = 110;
-let type = points > 100 ? 'gold' : 'silver';
-console.log(type);
-console.log(points > 100 ? 'gold' : 'silver');
+// let points = 110;
+// let type = points > 100 ? 'gold' : 'silver';
+// console.log(type);
+// console.log(points > 100 ? 'gold' : 'silver');
+
+// let x = 10;
+// let y = x++;
+// console.log(y);
+
+//Exercise for operators
+//Write code to swap values;
+// let a = 'red';
+// let b = 'blue';
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a);
+// console.log(b);
